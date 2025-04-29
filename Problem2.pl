c778ef33d465c38f8b641b45d6a30e983f1d9172
Shahd Elnassag <115586824+SH-code12@users.noreply.github.com>
2025-04-30 01:15:07 +0300

    Final Version Of Problem2 Problem2.pl

--- Problem2.pl
+++ Problem2.pl
@@ -1,20 +1,41 @@
-:- dynamic obs/2, del/2, grid_s/2.
-
-grid_s(5, 5).
+:- dynamic obs/2, del/2, grid_s/2,recharge/2,get_energy/1.
 
 
+% test Cases
+% case 1 Energy = 10. Short and Long
+%grid_s(4, 4).
+%obs(2, 2).
+%del(1, 4).
+%recharge(3,1).
+
+% case 2 Long and short but long reach the goal , Energy = 6.
+%grid_s(5, 5).
+%obs(2, 3).
+%obs(2, 3).
+%del(5, 5).
+%recharge(3,1).
+
+% Case 3 No Solution, Energy = 4.
+%grid_s(4, 4).
+%obs(2,2).
+%obs(2,3).
+%obs(3,2).
+%obs(3,3).
+%del(4, 4).
+
+% case 4 have same solution , Energy = 6.
+grid_s(3, 3).
 obs(2, 2).
-obs(3, 4).
-obs(5, 3).
 
 del(1, 3).
-del(4, 4).
-del(5, 5).
+del(3, 1).
+recharge(2,3).
+
 
 move((X,Y), (NWX,Y), 1) :- NWX is X + 1, valid(NWX, Y). % right
-move((X,Y), (X,NWY), 1) :- NWY is Y + 1, valid(X, NWY). % down
+move((X,Y), (X,NWY), 1) :- NWY is Y + 1, valid(X, NWY). % up
 move((X,Y), (NWX,Y), 1) :- NWX is X - 1, valid(NWX, Y). % left
-move((X,Y), (X,NWY), 1) :- NWY is Y - 1, valid(X, NWY). % up
+move((X,Y), (X,NWY), 1) :- NWY is Y - 1, valid(X, NWY). % down
 
 % In the correct range or not
 valid(X, Y) :-
@@ -32,57 +53,54 @@ manhattan_dis((X1,Y1), (X2,Y2), D) :-
     D is abs(X1 - X2) + abs(Y1 - Y2).
 
 % A* Search
-astar_search(Start, Path) :-
+astar_search(Start, Path,Energy) :-
     get_all_delivery_points(AllPoints),
-    search([node(Start, AllPoints, [], 0, 0)], [], RevPath),
-    reverse(RevPath, Path).
+    search([node(Start, AllPoints, [], 0, 0,Energy)], [], Path,Energy).
 
-% Base case>>>>finished all delevry points
-search([node(Pos, [], Path, _, _)|_], _, [Pos|Path]) :- !.
+% No Path Found
+search([],_,_,_):-
+   !, write(' No Solutuion Found , False'), nl , fail.
+
+% All Delivery Collected
+search([node(Pos, [], Path, _, _, _)|_], _, FullPath,_) :-
+    reverse([Pos|Path],FullPath), !.
 
 % Recursive case
-search([Node|Open], Closed, Sol) :-
-    Node = node(Pos, Rem, Path, G, _),
+search([Node|Open], Closed, Sol,MxEnergy) :-
+    Node = node(Pos, Rem, Path, G, _,En),
     findall(
-        node(NwPos, NwRem, [Pos|Path], NwG, NwF),
+        node(NwPos, NwRem, [Pos|Path], NwG, NwF,NwE),
         (
             move(Pos, NwPos, MoveCost),
+            En >= MoveCost, % check if have enough energy
             (member(NwPos, Rem) ->
                 select(NwPos, Rem, NwRem) ;
                 NwRem = Rem
             ),
+            % Recharge if find recharge station
+            NwPos = (NX,NY),
+           (recharge(NX,NY)-> NwE = MxEnergy;NwE is En - MoveCost),
             NwG is G + MoveCost,
             calc_h(NwPos, NwRem, H),
             NwF is NwG + H,
-            \+ member(node(NwPos, NwRem, _, _, _), Closed)
+            \+ member(node(NwPos, NwRem, _, _, _, _), Closed)
         ),
         Children
     ),
     append(Open, Children, NwOpen),
     predsort(compare_f, NwOpen, SortedOpen),
-    search(SortedOpen, [Node|Closed], Sol).
+    search(SortedOpen, [Node|Closed], Sol,MxEnergy).
 
 % compare functions so we can sort
-compare_f(<, node(_,_,_,_,F1), node(_,_,_,_,F2)) :- F1 < F2.
-compare_f(>, node(_,_,_,_,F1), node(_,_,_,_,F2)) :- F1 > F2.
-compare_f(=, _, _).
+compare_f(<, node(_,_,_,_,F1,_), node(_,_,_,_,F2,_)) :- F1 < F2.
+compare_f(>, node(_,_,_,_,F1,_), node(_,_,_,_,F2,_)) :- F1 > F2.
+compare_f(=, _, _):- !.
 
 
 get_all_delivery_points(Points) :-
     findall((X,Y), del(X,Y), Points).
 
-% Main execution
-:- initialization(main).
-
-
-main:-
-    (astar_search((1,1), Path) ->
-        format('Optimal path found:~n'),
-        print_path(Path),
-        visualize_grid(Path)
-    ;
-        write('No path found! Check obstacles and delivery points.')
-    ).
+% Ptint Final Path
 
 print_path([]).
 print_path([(X,Y)|Rest]) :-
@@ -91,13 +109,41 @@ print_path([(X,Y)|Rest]) :-
 
 visualize_grid(Path) :-
     grid_s(M, N),
-    format('~nGrid Visualization (D=Path, O=Obstacle, P=Delivery):~n'),
-    between(1, N, Y),
-    nl,
-    between(1, M, X),
-    (member((X,Y), Path) -> write(' D ');
-     obs(X,Y) -> write(' O ');
-     del(X,Y) -> write(' P ');
-     write(' . ')),
+    format('~nGrid Visualization (D=Path, O=Obstacle, P=Delivery, R=Recharge):~n'),
+    forall(between(1, N, Y),
+       (
+           nl,
+           forall(between(1, M, X),
+              (
+                  (recharge(X,Y)-> write(' R ')
+                  ;member((X,Y), Path) -> write(' D ')
+                  ; obs(X,Y) -> write(' O ')
+                  ; del(X,Y) -> write(' P ')
+                  ; write(' . '))
+              )
+           )
+       )
+    ),
+    nl.
+
+% Main execution
+:- initialization(main).
+
+
+main:-
+    write('Enter Starting Energy: '),
+    read(Energy),
+    (integer(Energy), Energy > 0 ->
+    retractall(get_energy(_)),
+    asserta(get_energy(Energy)),
+    (astar_search((1,1), Path,Energy) ->
+        format('Optimal path found:~n'),
+        print_path(Path),nl,
+        visualize_grid(Path)
+            ;
+            true
+        )
+    ;
+        write('Invalid energy value. Please enter a positive integer.'), nl
+    ),
     fail.
-visualize_grid(_) :- nl.
